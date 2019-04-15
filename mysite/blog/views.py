d493d5d5ae2eeecdf9d65379df18478cb0c6b4b2
Chiudk <weixiaosmile010203@outlook.com>
2019-04-15 23:16:43 +0800

    添加BLOG的分页功能

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from .models import Blog, BlogType
+from django.core.paginator import Paginator
 
 
 def blog_list(request):
+    page_num = request.GET.get('page', 1)#获取URL的页面参数（GET请求）
+    blogs_all_list = Blog.objects.all()
+    paginator = Paginator(blogs_all_list, 10)
+    page_of_blogs = paginator.get_page(page_num)
+
+
+
     context = {}
-    context['blogs'] = Blog.objects.all()
+    context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog_list.html', context)
 
