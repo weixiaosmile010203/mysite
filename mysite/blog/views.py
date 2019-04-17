71aeb5e1ca0317ae9845b6e642bd2e0851d78292
Chiudk <weixiaosmile010203@outlook.com>
2019-04-17 18:36:33 +0800

    优化分页-显示众多分页中的一部分

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,12 +4,22 @@ from django.core.paginator import Paginator
 
 
 def blog_list(request):
-    page_num = request.GET.get('page', 1)#获取URL的页面参数（GET请求）
+    page_num = int(request.GET.get('page', 1))#获取URL的页面参数（GET请求）
     blogs_all_list = Blog.objects.all()
     paginator = Paginator(blogs_all_list, 10)
     page_of_blogs = paginator.get_page(page_num)
+    if paginator.num_pages > 7:
+        if page_num - 3 < 1:
+            page_ran = range(1, 8)
+        elif page_num + 3 > paginator.num_pages:
+            page_ran = range(paginator.num_pages - 6, paginator.num_pages + 1)
+        else:
+            page_ran = range(page_num - 3, page_num + 4)
+        page_range = page_ran
+    else:
+        page_range = paginator.page_range
     context = {}
-    # context['blogs'] = page_of_blogs.object_list
+    context['page_range'] = page_range
     context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog_list.html', context)
