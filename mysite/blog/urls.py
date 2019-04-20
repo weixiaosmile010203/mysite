a077afd47c693fc2964bbb2c4caa3256aa38bd3b
Chiudk <weixiaosmile010203@outlook.com>
2019-04-21 00:20:03 +0800

    添加月份分类

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.blog_list, name='blog_home'),
     path('<int:blog_pk>', views.blog_detail, name='blog_detail'),
-    path('type/<int:blog_type_pk>', views.blog_with_type, name='blog_with_type')
+    path('type/<int:blog_type_pk>', views.blog_with_type, name='blog_with_type'),
+    path('date/<int:year>/<int:month>', views.blog_with_date, name='blog_with_date'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -29,11 +29,11 @@ def blog_list(request):
         page_range.insert(0, 1)
     if page_range[-1] != paginator.num_pages:
         page_range.append(paginator.num_pages)
-
     context = {}
     context['page_range'] = page_range
     context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
+    context['blog_dates'] = Blog.objects.dates('created_time', 'month', order="DESC")
     return render_to_response('blog_list.html', context)
 
 
@@ -47,6 +47,7 @@ def blog_detail(request, blog_pk):
 
 
 def blog_with_type(request, blog_type_pk):
+    context = {}
     page_num = request.GET.get('page', 1)  # 获取URL的页面参数（GET请求）
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     blogs_all_list = Blog.objects.filter(blog_type=blog_type)
@@ -74,10 +75,47 @@ def blog_with_type(request, blog_type_pk):
     if page_range[-1] != paginator.num_pages:
         page_range.append(paginator.num_pages)
 
-    context = {}
+
     context['blog_type'] = blog_type
     context['blogs'] = blogs_all_list
     context['page_range'] = page_range
     context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog_with_type.html', context)
+
+
+def blog_with_date(request, year, month):
+    context = {}
+    blogs_all_list = Blog.objects.filter(created_time__year=year, created_time__month=month)
+    paginator = Paginator(blogs_all_list, 10)
+    page_num = request.GET.get('page', 1)  # 获取URL的页面参数（GET请求）
+    page_of_blogs = paginator.get_page(page_num)
+    currentr_page_num = page_of_blogs.number
+    if paginator.num_pages > 7:
+        if currentr_page_num - 3 < 1:
+            page_ran = range(1, 8)
+        elif currentr_page_num + 3 > paginator.num_pages:
+            page_ran = range(paginator.num_pages - 6, paginator.num_pages + 1)
+        else:
+            page_ran = range(currentr_page_num - 3, currentr_page_num + 4)
+        page_range = list(page_ran)
+    else:
+        page_range = list(paginator.page_range)
+
+    if page_range[0] - 1 > 1:
+        page_range.insert(0, '...')
+    if paginator.num_pages - page_range[-1] > 1:
+        page_range.append('...')
+
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
+
+    context['blogs'] = blogs_all_list
+    context['page_range'] = page_range
+    context['page_of_blogs'] = page_of_blogs
+    context['blog_types'] = BlogType.objects.all()
+    context['blog_with_date'] = "%s年%s月" % (year, month)
+    context['blog_dates'] = Blog.objects.dates('created_time', 'month', order="DESC")
+    return render_to_response('blog_with_date.html', context)
